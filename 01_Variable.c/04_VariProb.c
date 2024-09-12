dafb98fa81121d321ca28b90e49c8d5c1f98d35d
Sarthak001G <sarthakgautam60@gmail.com>
2024-09-12 19:30:32 +0530

    Create 04_VariProb.c

---
+++ 01_Variable.c/04_VariProb.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+
+int main(){
+    int r;
+    printf("Enter the radius:\n");
+    scanf("%d", &r);
+
+    printf("The Area of circle radius %d is %d",r,3.14*r*r);
+    
+
+    return 0;
+}
