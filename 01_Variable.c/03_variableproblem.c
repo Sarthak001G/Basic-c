0392576fd4ad9f16a07b89ad21530480b50adc2a
Sarthak001G <sarthakgautam60@gmail.com>
2024-09-12 19:30:17 +0530

    Create 03_variableproblem.c

---
+++ 01_Variable.c/03_variableproblem.c
@@ -0,0 +1,17 @@
+#include<stdio.h> 
+
+int main(){
+    //int length=50;
+    //int breadth=70;
+
+    int length, breadth;
+
+
+    printf("Enter the length:\n");
+    scanf("%d", &length);
+
+    printf("Enter the length:\n");
+    scanf("%d", &breadth);
+    printf(" Area of the Given Length and breadth is : %d", length*breadth);
+    return 0;
+}
