15f53c9bb9063659f1390ac1e22db5f6a8c68303
Sarthak001G <sarthakgautam60@gmail.com>
2024-09-18 23:27:10 +0530

    Today learnt new thing in c on conditional statement

---
+++ 03_Conditional/05_Problem.c
@@ -0,0 +1,30 @@
+#include<stdio.h> 
+
+int main(){
+    char ch;
+    printf("Enter the character:");
+    scanf("%c", &ch);
+    if(ch>=97 && ch>=122){
+        printf("The character is lower case %d",ch);
+    }
+    else{
+        printf("The character is upper case:%d", ch);
+    }
+    return 0;
+}
+/*#include <stdio.h>
+
+int main() {
+    char ch;
+    printf("Enter the character: ");
+    scanf("%c", &ch);  // Use %c to scan a character
+
+    if (ch >= 97 && ch <= 122) {  // Check if it's a lowercase letter
+        printf("The character is lowercase. ASCII value: %d\n", ch);  // Print ASCII value using %d
+    } else {
+        printf("The character is uppercase or non-lowercase. ASCII value: %d\n", ch);
+    }
+
+    return 0;
+}
+*/
