b0ccb9b67d35b17553e2123d62b3d5230c6bbbfe
Sarthak <sarthakgautam60@gmail.com>
2024-10-08 22:47:48 +0530

    commit all

--- 01_Variable.c/05_VariProb.c
+++ 01_Variable.c/05_VariProb.c
@@ -1,4 +1,4 @@
-#include<stdio.h> 
+/*#include<stdio.h> 
 
 int main(){
     int r,h;
@@ -13,4 +13,20 @@ int main(){
     
 
     return 0;
+}*/
+#include <stdio.h>
+
+int main(){
+    int r, h;
+
+    printf("Enter the radius of the cylinder:\n");
+    scanf("%d", &r);
+
+    printf("Enter the height of the cylinder:\n");
+    scanf("%d", &h);
+
+    printf("The Volume of cylinder with radius %d and height %d is %f\n", r, h, 3.14 * r * r * h);
+
+    return 0;
 }
+
