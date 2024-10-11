4c48982ffd9e60794d5d90f82bb341a007d98b82
Sarthak <sarthakgautam60@gmail.com>
2024-10-11 15:45:17 +0530

    commit today work

--- 04.1_loop_practceset.c/03_P.c
+++ 04.1_loop_practceset.c/03_P.c
@@ -1,21 +1,37 @@
 #include<stdio.h>
 
 int main(){
-    int i=1,n,sum=0;
-    printf(" enter the natural number till which the sum of natural number to be printed:");
-    scanf("%d",&n);
-    if(n<=0){
-        printf("Given number is not natural number");
-    }else{
-        while (i<n)
-        {
-            sum+=i;
-        }
-        i++;
+    int i = 1, n, sum = 0;
+    /*printf("Enter the natural number till which the sum of natural numbers is to be printed: ");
+    scanf("%d", &n);
 
+    // Check if the input is a valid natural number
+    if (n <= 0) {
+        printf("Given number is not a natural number.\n");
+    } else {
+        // Loop to calculate the sum of natural numbers
+        while (i <= n) {
+            sum += i;
+            i++;  // Increment the counter
+        }
+        // Output the sum
+        printf("The sum of the first %d natural numbers is: %d\n", n, sum);
         
-    }
-    printf("sum of %d natural number is;%d ",n,sum);
+        }
+
+
+        while(i<=10){
+            sum +=i;
+            i++;
+
+        }
+         printf("The sum of the first %d natural numbers is: %d\n", n, sum);*/
+         do{
+            sum +=i;
+            i++;
+
+        }while(i<=10);
+         printf("The sum of the first %d natural numbers is: %d\n", n, sum);
 
     return 0;
 }
