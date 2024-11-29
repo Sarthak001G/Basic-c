fbb7cb610a9fe395f8f987d52e120042e0340aae
Sarthak <sarthakgautam60@gmail.com>
2024-11-30 00:11:59 +0530

    practice

--- 07_Pionter.c/06_p.c
+++ 07_Pionter.c/06_p.c
@@ -1,20 +1,30 @@
 #include<stdio.h>
+
+// Function declarations
 int multiply(int);
-int multiply(int a){
-	
-    return (a*2);
+int multiplyptr(int*);
+
+// Function definitions
+int multiply(int a) {
+    return (a * 2);
 }
 
+int multiplyptr(int* a) {
+    return (*a * 4);
+}
 
-int main(){
-    int i=2;
-    int* ptr=&i;
-    printf("Address of i is %u", &i);
-    printf("\nthe value is:\n%d",*ptr);
-    printf("\nthe value of i*2 is %d",multiply(i));
-    *ptr=i*3;
-    printf("\nThe value is:\n%d",*ptr);
+int main() {
+    int i = 2;
+    int* ptr = &i;
 
+    printf("Address of i is: %p", (void*)&i);
+    printf("\nValue of i using pointer: %d", *ptr);
+
+    printf("\nThe value of i * 2 is: %d", multiply(i));
+    printf("\nThe value of i * 4 (using pointer): %d", multiplyptr(ptr)); // Pass ptr
+
+    *ptr = i * 3;
+    printf("\nThe value of i after i * 3 (modified via pointer): %d\n", *ptr);
 
     return 0;
 }
